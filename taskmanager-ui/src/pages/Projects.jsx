768a5213655580274cdb1c7369aaf6ae1f528ca8
Pavankumar Pattar <158155527+Pavankumar1299@users.noreply.github.com>
2026-03-28 00:45:47 +0530

    Delete taskmanager-ui directory

--- taskmanager-ui/src/pages/Projects.jsx
+++
@@ -1,4 +0,0 @@
-function Projects() {
-  return <h1 className="p-6">Projects Page</h1>;
-}
-export default Projects;
--- taskmanager-ui/src/pages/Settings.jsx
+++
@@ -1,4 +0,0 @@
-function Settings() {
-  return <h1 className="p-6">Settings Page</h1>;
-}
-export default Settings;
